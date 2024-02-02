596952e630e010c4b936a72b72f736051a81b86b
FidelMartine <107603716+FidelMartine@users.noreply.github.com>
2024-02-01 22:29:21 -0600

    Update Rick.js

--- Rick.js
+++ Rick.js
@@ -1,16 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
-        itemList.appendChild(clone);
-    });
-    document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
-    });
+const charactersEl = document.getElementById('characters');
+const nameFilterEl = document.getElementById('name-filter');
+const statusFilterEl = document.getElementById('status-filter');
+
+// tenemos que crear la funcion que haga el llamado a la api
+
+async function getCharacters (name, status){
+
+    let url = 'https://rickandmortyapi.com/api/character/';
+
+    if (name || status){
+        url += '?';
+        if(name){
+            url += `name=${name}&`;
+        }
+
+        if(status){
+            url += `status=${status}`;
+        }
+    }
+
+
+    const response = await fetch(url);
+    const data = await response.json(); 
+
+    return data.results;
+
+}
+
+// la funcion que va a renderizar los elementos dentro del DOM
+
+async function displayCharacters (name, status) {
+    
+    // Obtener los personajes filtrados
+    const characters = await getCharacters(name, status);
+    
+    charactersEl.innerHTML = '';
+
+    // renderizar los personajes
+    for( let character of characters ){
+        const card = document.createElement('div');
+        card.classList.add('character-card');
+
+        card.innerHTML = `
+            <img src="${character.image}" />
+            <h2> ${character.name} </h2>
+            <p> Status: ${character.status} </p>
+            <p> Especie: ${character.species} </p>
+        `;
+
+        charactersEl.appendChild(card);
+
+    }
+
+}
+
+displayCharacters();
+
+nameFilterEl.addEventListener('input', () => {
+    displayCharacters(nameFilterEl.value, statusFilterEl.value );
+});
+
+statusFilterEl.addEventListener('change', () => {
+    displayCharacters(nameFilterEl.value, statusFilterEl.value );
 });
